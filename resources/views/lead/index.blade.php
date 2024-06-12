9a00d937ca87f576c589e8930cbf7abac1d5c882
developer <dimpyofficial44@gmail.com>
2024-06-12 17:15:23 +0530

    Changes on opportunities form

--- resources/views/lead/index.blade.php
+++ resources/views/lead/index.blade.php
@@ -21,7 +21,7 @@ $proposalstatus = \App\Models\Lead::$status;
 @section('action-btn')
 
 @can('Create Lead')
-<a href="#" data-url="{{ route('lead.create',['lead',0]) }}" data-size="lg" data-ajax-popup="true" data-bs-toggle="tooltip" data-title="{{__('New Opportunity
+<a href="#" data-url="{{ route('lead.create',['lead',0]) }}" data-size="lg" data-ajax-popup="true" data-bs-toggle="tooltip" data-title="{{__('Create New Opportunity
 ')}}" title="{{__('Create')}}" class="btn btn-sm btn-primary btn-icon m-1">
     <i class="ti ti-plus"></i>
 </a>
