60df6a10be667baac774aae17cedc2eda9690202
developer <dimpyofficial44@gmail.com>
2024-06-17 10:16:44 +0530

    Work on Move Company Name to the start of the row and Rename Name to Primary Contact on clients form

--- resources/views/customer/allcustomers.blade.php
+++ resources/views/customer/allcustomers.blade.php
@@ -31,18 +31,20 @@
                                     <table class="table datatable" id="datatable">
                                         <thead>
                                             <tr>
-                                                <th scope="col" class="sort" data-sort="primary_name">{{__('Name')}} <span class="opticy"> </span></th>
+                                                <th scope="col" class="sort">{{__('Company Name')}} <span class="opticy"> </span></th>
+                                                <th scope="col" class="sort" data-sort="primary_name">{{__('Primary Contact')}} <span class="opticy"> </span></th>
                                                 <th scope="col" class="sort" data-sort="primary_email">{{__('Email')}} <span class="opticy"> </span></th>
                                                 <th scope="col" class="sort">{{__('Phone Number')}} <span class="opticy"> </span></th>
                                                 <th scope="col" class="sort">{{__('Address')}} <span class="opticy"> </span></th>
                                                 <th scope="col" class="sort">{{__('Title/Designation')}} <span class="opticy"> </span></th>
-                                                <th scope="col" class="sort">{{__('Company Name')}} <span class="opticy"> </span></th>
+
                                                 <th scope="col" class="sort">{{__('Entity Name')}} <span class="opticy"> </span></th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             @foreach($importedcustomers as $customers)
                                             <tr>
+                                                <td>{{ucfirst($customers->company_name)}}</td>
                                                 <td>
                                                     <a href="{{route('customer.info',urlencode(encrypt($customers->id)))}}" title="{{ __('Client Details') }}" class="action-item text-primary" style="color:#1551c9 !important;">
                                                         <b> {{ucfirst($customers->primary_name)}}</b>
@@ -52,7 +54,6 @@
                                                 <td>{{ucfirst($customers->primary_phone_number)}}</td>
                                                 <td>{{ucfirst($customers->primary_address)}}</td>
                                                 <td>{{ucfirst($customers->primary_organization)}}</td>
-                                                <td>{{ucfirst($customers->company_name)}}</td>
                                                 <td>{{ucfirst($customers->entity_name)}}</td>
                                             </tr>
                                             @endforeach
