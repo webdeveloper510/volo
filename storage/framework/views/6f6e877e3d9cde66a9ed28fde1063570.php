2560988c4424bef4b23620998f40982cf16ea5f2
developer <dimpyofficial44@gmail.com>
2024-06-25 20:52:04 +0530

    Changes

--- storage/framework/views/6f6e877e3d9cde66a9ed28fde1063570.php
+++ storage/framework/views/6f6e877e3d9cde66a9ed28fde1063570.php
@@ -1,23 +1,25 @@
-@extends('layouts.admin')
-@section('page-title')
-{{__('Categories')}}
-@endsection
-@section('title')
+
+<?php $__env->startSection('page-title'); ?>
+<?php echo e(__('Categories')); ?>
+
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('title'); ?>
 <div class="page-header-title">
-    {{__('Categories')}}
+    <?php echo e(__('Categories')); ?>
+
 </div>
-@endsection
-@section('action-btn')
-<a href="#" data-bs-toggle="modal" data-bs-target="#createCategoryModal" data-bs-toggle="tooltip" data-title="{{__('Create category')}}" title="{{__('Create')}}" class="btn btn-sm btn-primary btn-icon m-1">
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('action-btn'); ?>
+<a href="#" data-bs-toggle="modal" data-bs-target="#createCategoryModal" data-bs-toggle="tooltip" data-title="<?php echo e(__('Create category')); ?>" title="<?php echo e(__('Create')); ?>" class="btn btn-sm btn-primary btn-icon m-1">
     <i class="ti ti-plus"></i>
 </a>
-@endsection
+<?php $__env->stopSection(); ?>
 
-@section('breadcrumb')
-<li class="breadcrumb-item"><a href="{{ route('dashboard') }}">{{__('Dashboard')}}</a></li>
-<li class="breadcrumb-item">{{__('Categories')}}</li>
-@endsection
-@section('content')
+<?php $__env->startSection('breadcrumb'); ?>
+<li class="breadcrumb-item"><a href="<?php echo e(route('dashboard')); ?>"><?php echo e(__('Dashboard')); ?></a></li>
+<li class="breadcrumb-item"><?php echo e(__('Categories')); ?></li>
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('content'); ?>
 
 <div class="container-field">
     <div id="wrapper">
@@ -31,23 +33,23 @@
                                     <table class="table datatable" id="datatable">
                                         <thead>
                                             <tr>
-                                                <th scope="col" class="sort">{{__('Sr No.')}} <span class="opticy"></span></th>
-                                                <th scope="col" class="sort" data-sort="name">{{__('Category Name')}} <span class="opticy"></span></th>
-                                                <th scope="col" class="sort">{{__('Created On')}}<span class="opticy"></span></th>
-                                                <!-- <th scope="col" class="sort">{{__('Action')}} <span class="opticy"></span></th> -->
+                                                <th scope="col" class="sort"><?php echo e(__('Sr No.')); ?> <span class="opticy"></span></th>
+                                                <th scope="col" class="sort" data-sort="name"><?php echo e(__('Category Name')); ?> <span class="opticy"></span></th>
+                                                <th scope="col" class="sort"><?php echo e(__('Created On')); ?><span class="opticy"></span></th>
+                                                <!-- <th scope="col" class="sort"><?php echo e(__('Action')); ?> <span class="opticy"></span></th> -->
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            @foreach($allCategory as $category)
+                                            <?php $__currentLoopData = $allCategory; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                                             <tr>
-                                                <td>{{ $loop->iteration }}</td>
-                                                <td>{{$category->name}}</td>
-                                                <td>{{ $category->created_at->format('F j, Y') }}</td>
+                                                <td><?php echo e($loop->iteration); ?></td>
+                                                <td><?php echo e($category->name); ?></td>
+                                                <td><?php echo e($category->created_at->format('F j, Y')); ?></td>
                                                 <!-- <td>
                                                     
                                                 </td> -->
                                             </tr>
-                                            @endforeach
+                                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                                         </tbody>
                                     </table>
                                 </div>
@@ -65,20 +67,20 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="createCategoryModalLabel">{{ __('Create Category') }}</h5>
+                <h5 class="modal-title" id="createCategoryModalLabel"><?php echo e(__('Create Category')); ?></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <div id="message" class="alert" style="display: none;"></div>
                 <form id="createCategoryForm">
-                    @csrf
+                    <?php echo csrf_field(); ?>
                     <div class="mb-3">
-                        <label for="categoryName" class="form-label">{{ __('Category Name') }}</label>
+                        <label for="categoryName" class="form-label"><?php echo e(__('Category Name')); ?></label>
                         <input type="text" class="form-control" id="categoryName" name="categoryName" required>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">{{ __('Close') }}</button>
-                        <button type="submit" class="btn btn-primary">{{ __('Save') }}</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><?php echo e(__('Close')); ?></button>
+                        <button type="submit" class="btn btn-primary"><?php echo e(__('Save')); ?></button>
                     </div>
                 </form>
             </div>
@@ -86,7 +88,7 @@
     </div>
 </div>
 
-@endsection
+<?php $__env->stopSection(); ?>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
@@ -97,10 +99,10 @@
             let categoryName = $('#categoryName').val();
 
             $.ajax({
-                url: '{{ route("categories.create") }}',
+                url: '<?php echo e(route("categories.create")); ?>',
                 method: 'POST',
                 data: {
-                    _token: '{{ csrf_token() }}',
+                    _token: '<?php echo e(csrf_token()); ?>',
                     categoryName: categoryName
                 },
                 success: function(response) {
@@ -118,3 +120,4 @@
         });
     });
 </script>
+<?php echo $__env->make('layouts.admin', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\volo\resources\views/category/index.blade.php ENDPATH**/ ?>
