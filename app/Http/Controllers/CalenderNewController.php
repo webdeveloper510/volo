3ff1d73e3fbd1e8476267d295fda63e99f403698
developer <dimpyofficial44@gmail.com>
2024-07-01 19:21:02 +0530

    Changes on calenderNewController

--- app/Http/Controllers/CalenderNewController.php
+++ app/Http/Controllers/CalenderNewController.php
@@ -18,9 +18,6 @@ class CalenderNewController extends Controller
         $blockeddate = Blockdate::all();
         return view('calender_new.index',compact('blockeddate'));
     }
-    public function index(){
-        
-    }
     public function get_event_data(Request $request)
     {
         $events = Meeting::where('start_date', $request->start)->get();
