4ca420282cf517a85d6d432614837ee7eaf30af6
developer <dimpyofficial44@gmail.com>
2024-08-30 10:36:17 +0530

    Rename Legal Entity Name to Client Name on create new opportunity page

---
+++ storage/framework/views/1dc793851a54ca7caae14b12e13dc76f.php
@@ -0,0 +1,6 @@
+<li class="dash-item dash-hasmenu <?php echo e((Request::route()->getName() == 'landingpage.index') ? ' active' : ''); ?>">
+    <a href="<?php echo e(route('landingpage.index')); ?>" class="dash-link">
+        <span class="dash-micon"><i class="ti ti-license"></i></span><span class="dash-mtext"><?php echo e(__('Landing Page')); ?></span>
+    </a>
+</li>
+<?php /**PATH C:\xampp\htdocs\volo\Modules/LandingPage\Resources/views/menu/landingpage.blade.php ENDPATH**/ ?>
